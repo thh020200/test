b7fbee6dfe1f0b35cd0cd40b2f05c744428dbaf6
thh020200 <tranhoanghuy27101999@gmail.com>
2021-03-24 14:07:57 +0700

    them password

--- baitap/baitap/Form1.Designer.cs
+++ baitap/baitap/Form1.Designer.cs
@@ -29,45 +29,65 @@ namespace baitap
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txt = new System.Windows.Forms.Label();
+            this.lbUsername = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // label1
+            // txt
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(389, 36);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(37, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Login";
+            this.txt.AutoSize = true;
+            this.txt.Location = new System.Drawing.Point(389, 36);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(37, 15);
+            this.txt.TabIndex = 0;
+            this.txt.Text = "Login";
             // 
-            // label2
+            // lbUsername
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(158, 72);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 15);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Username";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.lbUsername.AutoSize = true;
+            this.lbUsername.Location = new System.Drawing.Point(158, 72);
+            this.lbUsername.Name = "lbUsername";
+            this.lbUsername.Size = new System.Drawing.Size(60, 15);
+            this.lbUsername.TabIndex = 1;
+            this.lbUsername.Text = "Username";
+            this.lbUsername.Click += new System.EventHandler(this.label2_Click);
             // 
-            // textBox1
+            // txtUsername
             // 
-            this.textBox1.Location = new System.Drawing.Point(260, 72);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(166, 23);
-            this.textBox1.TabIndex = 2;
+            this.txtUsername.Location = new System.Drawing.Point(260, 72);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(166, 23);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(158, 140);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(57, 15);
+            this.lbPassword.TabIndex = 3;
+            this.lbPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(260, 137);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(166, 23);
+            this.txtPassword.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lbUsername);
+            this.Controls.Add(this.txt);
             this.Name = "Form1";
             this.Text = "Username";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -78,9 +98,11 @@ namespace baitap
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label txt;
+        private System.Windows.Forms.Label lbUsername;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.TextBox txtPassword;
     }
 }
 
